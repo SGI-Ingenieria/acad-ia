867ecc53e0ed7c24e21110780b078d711e4ffa1b
Guillermo Arrieta Medina <guillermo.arrieta@lasallistas.org.mx>
2026-01-30 12:50:31 -0600

    Que se pueda escribir en el campo de Instrucciones adicionales para la IA
    fix #59:
    El bug se debía a que en la funcion onChange se hacia referencia al valor a cambiar con 'I' mayúscula en vez de minúscula.

--- src/components/planes/wizard/PasoDetallesPanel/PasoDetallesPanel.tsx
+++ src/components/planes/wizard/PasoDetallesPanel/PasoDetallesPanel.tsx
@@ -79,7 +79,7 @@ export function PasoDetallesPanel({
                 ...w,
                 iaConfig: {
                   ...(w.iaConfig || ({} as any)),
-                  InstruccionesAdicionalesIA: e.target.value,
+                  instruccionesAdicionalesIA: e.target.value,
                 },
               }))
             }
